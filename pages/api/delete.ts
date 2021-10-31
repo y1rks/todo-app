1a2f24f2ded6323fea95af87ed1f847695ab06e7
y1rks <yu.martian@gmail.com>
2021-10-31 23:03:16 +0900

    Add delete api

---
+++ pages/api/delete.ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { pool } from "../../lib/db";
+
+type Data = {
+  success: boolean;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  console.log(req.query.id);
+  if (req.query.id && typeof req.query.id === "number") {
+    try {
+      const sql = "delete from todo_table where id = $1";
+      const values = [req.query.id];
+
+      const client = await pool.connect();
+      const result = await client.query(sql, values);
+      client.release();
+
+      res.status(200).json({ success: true });
+    } catch (err) {
+      console.error(err);
+      res.status(200).json({ success: false });
+    }
+  } else {
+    res.status(200).json({ success: false });
+  }
+}
