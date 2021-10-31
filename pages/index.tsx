3b8a1a504796619459cc27233933b3bab6b21a58
y1rks <yu.martian@gmail.com>
2021-11-01 04:04:35 +0900

    Add types

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,15 @@ import styles from "../styles/Home.module.css";
 const SHOW_ALL_URL = "https://guarded-tundra-11745.herokuapp.com/api/showAll";
 const REGISTER_URL = "http://localhost:3000/api/register";
 
+type ShowAllResponse = {
+  results: Item[];
+};
+
+type Item = {
+  id: number;
+  content: string;
+};
+
 function registerTodo(content: string) {
   fetch(REGISTER_URL, {
     method: "POST",
@@ -20,22 +29,23 @@ function registerTodo(content: string) {
   });
 }
 
-async function fetchAllTodo() {
+async function fetchAllTodo(): Promise<ShowAllResponse> {
   const res = await fetch(SHOW_ALL_URL, { method: "GET" });
 
-  return res.json();
+  return res.json() as unknown as ShowAllResponse;
 }
 
 const Todo: NextPage = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([] as Item[]);
   const [content, setContent] = useState("");
 
   useEffect(() => {
     const func = async () => {
-      const res = await fetchAllTodo();
+      const res: ShowAllResponse = await fetchAllTodo();
       console.log("########");
       console.log("########");
-      console.log(res);
+      console.log(res.results);
+      setTodos(res.results);
     };
     func();
   });
