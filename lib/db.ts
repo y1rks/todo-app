c8f50dffb6c25fbb0c156cc7593a6b146c70fde6
y1rks <yu.martian@gmail.com>
2021-10-31 23:14:38 +0900

    .

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,7 @@
 import { Pool } from "pg";
 
-// create table todo_table (id integer, content text);
-// insert into todo_table values (1, 'Test todo');
-// insert into todo_table values (2, 'Just do it!');
+// create table todo_table (id integer generated always as identity, content text, primary key (id))
 
-// const { Pool } = require("pg");
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
