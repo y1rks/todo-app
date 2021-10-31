27dcd0cc3492375c813d915bf9117fcd80679b73
y1rks <yu.martian@gmail.com>
2021-10-31 19:47:21 +0900

    .

--- pages/api/create.ts
+++ pages/api/create.ts
@@ -9,15 +9,18 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  // DB connection
   try {
-    // const client = await pool.connect();
-    // const result = await client.query(
-    //   "insert into todo_table values (3, 'Test todo3')"
-    // );
+    const sql = "INSERT INTO todo_table(content) VALUES($1)";
+    const values = ["Test task!"];
+
+    const client = await pool.connect();
+    const result = await client.query(sql, values);
+    console.log(result.rows[0]);
+    client.release();
+
     // const results = { results: result ? result.rows : null };
     // console.log(results);
-    // client.release();
+    //
     res.status(200).json({ success: true });
   } catch (err) {
     console.error(err);
