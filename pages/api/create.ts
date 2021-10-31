bdf966ee49f7958cc95e17625388c87bc5e60980
y1rks <yu.martian@gmail.com>
2021-10-31 20:04:32 +0900

    .

--- pages/api/create.ts
+++ pages/api/create.ts
@@ -5,24 +5,28 @@ type Data = {
   success: boolean;
 };
 
+type ResponseBody = {
+  content: string;
+};
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  try {
-    const sql = "INSERT INTO todo_table(content) VALUES($1)";
-    const values = ["Test task!"];
+  if (req.body.content && typeof req.body.content === "string") {
+    try {
+      const sql = "INSERT INTO todo_table(content) VALUES($1)";
+      const values = [...req.body.content];
+      const client = await pool.connect();
+      const result = await client.query(sql, values);
+      client.release();
 
-    const client = await pool.connect();
-    const result = await client.query(sql, values);
-    console.log(result.rows[0]);
-    client.release();
-
-    // const results = { results: result ? result.rows : null };
-    // console.log(results);
-    //
-    res.status(200).json({ success: true });
-  } catch (err) {
-    console.error(err);
+      res.status(200).json({ success: true });
+    } catch (err) {
+      console.error(err);
+      res.status(200).json({ success: false });
+    }
+  } else {
+    res.status(200).json({ success: false });
   }
 }
